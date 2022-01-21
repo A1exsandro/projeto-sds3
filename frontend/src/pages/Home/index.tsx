20a84f1a37822781c679f18fee4399129f04a186
A1exsandro <alexsandro.acp@gmail.com>
2022-01-21 17:42:02 -0400

    Description

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -11,7 +11,7 @@ const Home = () => {
                     <h1 className="display-4">DSVendas</h1>
                     <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
                     <hr />
-                    <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
+                    <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com React e Spring Boot.</p>
                     <Link className="btn btn-primary btn-lg" to="/dashboard">Acessar Dashboard</Link>
                 </div>
             </div>
